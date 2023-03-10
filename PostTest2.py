a73836bc89b1229dd85520ce0bd0d7b0ed0cf3b6
Tsurayya Salsabila <122155316+salsabilayya@users.noreply.github.com>
2023-03-10 22:30:36 +0800

    Add files via upload
    https://laufey-area.notion.site/POST-TEST-2-Searching-fe9720710aaa4f5f8ee2704cb2714e72

---
+++ PostTest2.py
@@ -0,0 +1,21 @@
+arr = ['Arsel','Avivah','Daiva',['Wahyu','Wibi']]
+def linearsearch(arr,cari):
+    for l in range(len(arr)):
+        if type(arr[l]) == list:
+            hasil1 = linearsearch(arr[l],cari)
+            if hasil1 == -1:
+                return -1
+            else:
+                print(f'{cari} telah ditemukan pada indeks {l} pada kolom {hasil1}')
+                exit()
+        elif arr[l] == cari:
+            return l
+    return -1
+print(arr)
+
+cari = input('Masukan nama yang ingin dicari : ')
+hasil2 = linearsearch(arr,cari)
+if hasil2 == -1:
+    print(f'{cari} telah ditemukan pada indeks {hasil2}')
+else:
+    print(f'{cari} telah ditemukan pada indeks {hasil2}')
